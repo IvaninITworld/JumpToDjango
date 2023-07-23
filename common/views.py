6f7f870d79a11ab78110c555d81e520b7cf36154
ivan <xormrdlsrks2@gmail.com>
2023-07-23 16:48:50 +0900

    Add user tag on post and comment

--- common/views.py
+++ common/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+# authenticate : 사용자 인증 담당
+# login : 로그인 담당
 from django.contrib.auth import authenticate, login
 from common.forms import UserForm
 
@@ -6,15 +8,16 @@ from common.forms import UserForm
 # Create your views here.
 
 def signup(request):
-    if request.method == "POST":
+    if request.method == 'POST':
         form = UserForm(request.POST)
         if form.is_valid():
             form.save()
+            # form.cleaned_data.get : 폼의 입력값을 개별적으로 얻고 싶은 경우
             username = form.cleaned_data.get('username')
-            raw_password = form.cleaned_data.get('password1')
-            user = authenticate(username=username, password=raw_password)  # 사용자 인증
-            login(request, user)  # 로그인
+            raw_password = form.cleaned_data.get('password')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
             return redirect('index')
-    else:
-        form = UserForm()
-    return render(request, 'common/signup.html', {'form': form})
+        else:
+            form = UserForm()
+        return render(request, 'common/signup.html', {'form': form})
